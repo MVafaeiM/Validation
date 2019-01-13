9b011e219ade5413612615ea33a9ed93bdf8a4ca
MVafaeiM <mvmsoft7@gmail.com>
2019-01-13 11:55:16 +0330

    WithNetworkValidation

---
+++ Validation.UnitTest/NetwordTest.cs
@@ -0,0 +1,143 @@
+﻿using Validation.Network;
+using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Validation.UnitTest
+{
+    [TestClass]
+    public class NetwordTest
+    {
+        class Values
+        {
+            public string Value { set; get; }
+            public bool Expected { set; get; }
+
+        }
+
+        [TestMethod()]
+        public void CheckIPv4Test()
+        {
+            List<Values> values = new List<Values>
+            {
+                new Values() {Value = "192.168.1.1", Expected = true},
+                new Values() {Value = "1.1.1.1", Expected = true},
+                new Values() {Value = null, Expected = false},
+                new Values() {Value = "", Expected = false},
+                new Values() {Value = "ali", Expected = false},
+                new Values() {Value = "123", Expected = false}
+            };
+            //Arange 
+
+            foreach (var item in values)
+            {
+                //Actual 
+                var act = item.Value.CheckIPv4();
+                //Expected
+                Assert.AreEqual(act, item.Expected);
+            }
+        }
+
+        [TestMethod()]
+        public void CheckPingIpTest()
+        {
+            List<Values> values = new List<Values>
+            {
+                new Values() {Value = "192.168.1.1", Expected = false},
+                new Values() {Value = "1.1.1.1", Expected = false},
+                new Values() {Value = null, Expected = false},
+                new Values() {Value = "", Expected = false},
+                new Values() {Value = "ali", Expected = false},
+                new Values() {Value = "123", Expected = false},
+                new Values() {Value = "127.0.0.1", Expected = true}
+            };
+            //Arange 
+
+            foreach (var item in values)
+            {
+                //Actual 
+                var act = item.Value.CheckPingIp(500);
+                //Expected
+                Assert.AreEqual(act, item.Expected);
+            }
+        }
+
+        [TestMethod()]
+        public void CheckInternetConnectionTest()
+        {
+            //Arange 
+            //Actual 
+            var act = NetworkValidation.CheckInternetConnection();
+            //Expected
+            Assert.AreEqual(act, false);
+        }
+
+        [TestMethod()]
+        public void CheckEmailTest()
+        {
+            List<Values> values = new List<Values>
+            {
+                new Values() {Value = "mvmsoft7@gmail.com", Expected = true},
+                new Values() {Value = "1.1.1.1", Expected = false},
+                new Values() {Value = null, Expected = false},
+                new Values() {Value = "", Expected = false},
+                new Values() {Value = "ali", Expected = false},
+                new Values() {Value = "123", Expected = false},
+                new Values() {Value = "@test.com", Expected = false}
+            };
+            //Arange 
+
+            foreach (var item in values)
+            {
+                //Actual 
+                var act = item.Value.CheckEmail();
+                //Expected
+                Assert.AreEqual(act, item.Expected);
+            }
+        }
+
+        [TestMethod()]
+        public void CheckOpenedPortTest()
+        {
+            List<Values> values = new List<Values>
+            {
+                new Values() {Value = "8080", Expected = false},
+                new Values() {Value = "80", Expected = true},
+                new Values() {Value = "0", Expected = false},
+                new Values() {Value = "13", Expected = true},
+            };
+            //Arange 
+
+            foreach (var item in values)
+            {
+                //Actual 
+                var act = int.Parse(item.Value).CheckOpenedPort();
+                //Expected
+                Assert.AreEqual(act, item.Expected);
+            }
+        }
+
+        [TestMethod()]
+        public void CheckValidUrlTest()
+        {
+            List<Values> values = new List<Values>
+            {
+                new Values() {Value = "http://google.com", Expected = true},
+                new Values() {Value = "www.google.com", Expected = false},
+                new Values() {Value = "test.com", Expected = false},
+                new Values() {Value = "test", Expected = false},
+                new Values() {Value = "https://google.com", Expected = true},
+            };
+            //Arange 
+
+            foreach (var item in values)
+            {
+                //Actual 
+                var act = item.Value.CheckValidUrl();
+                //Expected
+                Assert.AreEqual(act, item.Expected);
+            }
+        }
+    }
+}
--- Validation/NetworkValidation.cs
+++ Validation/NetworkValidation.cs
@@ -6,6 +6,7 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
+using Validation.Public;
 
 namespace Validation
 {
@@ -15,12 +16,15 @@ namespace Validation
         {
             public static bool CheckIPv4(this string input)
             {
-                IPAddress address;
-                if (IPAddress.TryParse(input, out address))
+                if (input.CheckEmpty()) return false;
+                if (input == null) return false;
+                if (input.Contains(".") == false) return false;
+                if (IPAddress.TryParse(input, out var address))
                 {
                     if (address.AddressFamily == AddressFamily.InterNetwork)
                     {
-                        return true;
+                        if (address.GetAddressBytes().Length == 4)
+                            return true;
                     }
                 }
                 return false;
@@ -31,10 +35,9 @@ namespace Validation
                 try
                 {
                     Ping p = new Ping();
-                    PingReply r;
-                    String s = ip; //Replace input from your text box for IP address here
-                    r = p.Send(s, timeout);
-                    if (r.Status == IPStatus.Success)
+                    String s = ip; 
+                    var r = p.Send(s, timeout);
+                    if (r != null && r.Status == IPStatus.Success)
                     {
                         return true;
                     }
@@ -47,20 +50,27 @@ namespace Validation
             }
             public static bool CheckInternetConnection()
             {
-                bool networkUp = NetworkInterface.GetIsNetworkAvailable();
-                return networkUp;
+                    try
+                    {
+                        using (var client = new WebClient())
+                        using (client.OpenRead("http://google.com"))
+                        {
+                            return true;
+                        }
+                    }
+                    catch
+                    {
+                        return false;
+                    }
             }
             public static bool CheckEmail(this string mailAddress)
             {
+                if (mailAddress.CheckEmpty()) return false;
+                if (mailAddress == null) return false;
                 string strPattern = "^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
-                if (System.Text.RegularExpressions.Regex.IsMatch(mailAddress, strPattern))
+                if (Regex.IsMatch(mailAddress, strPattern))
                 { return true; }
                 return false;
-            }
-            public static bool CheckUrlValid(this string source)
-            {
-                Uri uriResult;
-                return Uri.TryCreate(source, UriKind.Absolute, out uriResult) && uriResult.Scheme == Uri.UriSchemeHttp;
             }
             public static bool CheckOpenedPort(this int port)
             {
@@ -76,8 +86,10 @@ namespace Validation
                 }
                 return false;
             }
-            public static bool IsValidUrl(this string url)
+            public static bool CheckValidUrl(this string url)
             {
+                if (url.CheckEmpty()) return false;
+                if (url == null) return false; 
                 Regex rx = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?");
                 return rx.IsMatch(url);
             }
